3bc0a383229f672a91637e76870eb8f294664012
furkanmtorun <furkanmtorun@gmail.com>
2019-07-25 22:59:08 +0300

    Adding flash messages and message category

--- codonio.py
+++ codonio.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, redirect, flash
 app = Flask(__name__)
+
+# Conversion from message type to color
+msg_type_to_color = {
+    'success': 'green lighten-1',
+    'error': 'red',
+    'warning': 'orange',
+    'announcement': 'purple lighten-1'
+}
 
 # Index Page
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
@@ -23,6 +31,13 @@ def profil():
 def settings():
     return render_template("settings.html")
 
+# Logout Page
+@app.route("/logout")
+def logout():
+    flash("You logged out!", msg_type_to_color['success'])
+    return redirect(url_for("home"))
+
 # codon.io
 if __name__ == "__main__":
+    app.secret_key = "qYqDyVT^!2%qUsY=A@qb"
     app.run(debug=True)
