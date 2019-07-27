37c9eed9b29399cf8227239c23b6ba5ac83e1a30
bilalkabas <bilalkabas@icloud.com>
2019-07-27 18:54:02 +0300

    Register and Log in forms validation added

--- codonio.py
+++ codonio.py
@@ -24,7 +24,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash("Registration was completed successfuly.", msg_type_to_color["success"])
-        return redirect(url_for("login"))
+        return redirect(url_for("login"))        
     return render_template("register.html", form=form, title="Register")
 
 # Login Page
--- forms.py
+++ forms.py
@@ -3,14 +3,21 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=35)])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField("Sign up")
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=100)])
+    
+    confirm_password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=100), EqualTo('password', message= 'Password do not match!')])
+
+    submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=35)])
-    password = PasswordField("Password", validators=[DataRequired()])
-    remember = BooleanField("Remember me")
-    submit = SubmitField("Login")
+    username = StringField('Username', validators=[DataRequired()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    remember = BooleanField('Remember me')
+
+    submit = SubmitField('Login')
