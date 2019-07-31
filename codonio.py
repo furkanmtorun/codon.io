3ff8c396abd95d6d4134df44ad38e10ed785c317
furkanmtorun <furkanmtorun@gmail.com>
2019-07-31 22:30:44 +0300

    Adding 404 Error Page

--- codonio.py
+++ codonio.py
@@ -193,6 +193,12 @@ def logout():
 
 
 
+# 404 Error Page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 # codon.io
 if __name__ == "__main__":
     app.run(debug=True)
